a57fb88918cf05b65e265d6ef9dcac6935e16130
IvoKirilov11 <mefistofel_1980@abv.bg>
2021-12-14 21:21:45 +0200

    Add project files.

---
+++ ProductShop/DataTransferObject/CategoryInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductShop.DataTransferObject
+{
+    public class CategoryInputModel
+    {
+
+        public string Name { get; set; }
+    }
+}
---
+++ ProductShop/ProductShopProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ProductShop.DataTransferObject;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<UserInputModel,User>();
+
+            CreateMap<ProductInputModel, Product>();
+
+            CreateMap<CategoryInputModel, Category>();
+
+            CreateMap<CategoryProductModel, CategoryProductModel>();
+        }
+    }
+}
